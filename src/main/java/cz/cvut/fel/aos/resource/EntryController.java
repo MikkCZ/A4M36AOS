da96a60ce1dab342e9a91c01c8f2486f85ddd294
Michal Stanke <stankmic@fel.cvut.cz>
2016-11-22 22:04:34 +0100

    #5 single entry point descriptions

--- src/main/java/cz/cvut/fel/aos/resource/EntryController.java
+++ src/main/java/cz/cvut/fel/aos/resource/EntryController.java
@@ -16,25 +16,25 @@ public class EntryController {
     public Map<String, Map<String, String>> singleEntryPoint() {
         return ImmutableMap.of(
                 "Destination", ImmutableMap.of(
-                        "GET /destination", "",
-                        "POST /destination", "",
-                        "GET /destination/{id}", "",
-                        "PUT /destination/{id}", "",
-                        "DELETE /destination/{id}", ""
+                        "GET /destination", "Return list of all destinations.",
+                        "POST /destination", "Crete new destination and return its id.",
+                        "GET /destination/{id}", "Return destination with given id.",
+                        "PUT /destination/{id}", "Update destination with given id.",
+                        "DELETE /destination/{id}", "Delete destination with given id."
                 ),
                 "Flight", ImmutableMap.of(
-                        "GET /flight", "",
-                        "POST /flight", "",
-                        "GET /flight/{id}", "",
-                        "PUT /flight/{id}", "",
-                        "DELETE /flight/{id}", ""
+                        "GET /flight", "Return list of all flights.",
+                        "POST /flight", "Crete new flight and return its id.",
+                        "GET /flight/{id}", "Return flight with given id.",
+                        "PUT /flight/{id}", "Update flight with given id.",
+                        "DELETE /flight/{id}", "Delete flight with given id."
                 ),
                 "Reservation", ImmutableMap.of(
-                        "GET /reservation", "",
-                        "POST /reservation", "",
-                        "GET /reservation/{id}", "",
-                        "PUT /reservation/{id}", "",
-                        "DELETE /reservation/{id}", ""
+                        "GET /reservation", "Return list of all reservations.",
+                        "POST /reservation", "Crete new reservation and return it with id and password filled.",
+                        "GET /reservation/{id}", "Return reservation with given id.",
+                        "PUT /reservation/{id}", "Update reservation with given id.",
+                        "DELETE /reservation/{id}", "Delete reservation with given id."
                 )
         );
     }
