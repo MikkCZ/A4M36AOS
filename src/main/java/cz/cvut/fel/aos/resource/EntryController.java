b123f2eae32e7a12bf9bc0e7a0d7373f7fce6569
Zbyněk Eiselt <eiselzby@fel.cvut.cz>
2016-11-24 00:11:56 +0100

    Kill me, jsp is not working

--- src/main/java/cz/cvut/fel/aos/resource/EntryController.java
+++ src/main/java/cz/cvut/fel/aos/resource/EntryController.java
@@ -13,29 +13,7 @@ import java.util.Map;
 public class EntryController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET, produces={MediaType.APPLICATION_JSON_VALUE})
-    public Map<String, Map<String, String>> singleEntryPoint() {
-        return ImmutableMap.of(
-                "Destination", ImmutableMap.of(
-                        "GET /destination", "Return list of all destinations.",
-                        "POST /destination", "Crete new destination and return its id.",
-                        "GET /destination/{id}", "Return destination with given id.",
-                        "PUT /destination/{id}", "Update destination with given id.",
-                        "DELETE /destination/{id}", "Delete destination with given id."
-                ),
-                "Flight", ImmutableMap.of(
-                        "GET /flight", "Return list of all flights.",
-                        "POST /flight", "Crete new flight and return its id.",
-                        "GET /flight/{id}", "Return flight with given id.",
-                        "PUT /flight/{id}", "Update flight with given id.",
-                        "DELETE /flight/{id}", "Delete flight with given id."
-                ),
-                "Reservation", ImmutableMap.of(
-                        "GET /reservation", "Return list of all reservations.",
-                        "POST /reservation", "Crete new reservation and return it with id and password filled.",
-                        "GET /reservation/{id}", "Return reservation with given id.",
-                        "PUT /reservation/{id}", "Update reservation with given id.",
-                        "DELETE /reservation/{id}", "Delete reservation with given id."
-                )
-        );
+    public String singleEntryPoint() {
+        return "index";
     }
 }
