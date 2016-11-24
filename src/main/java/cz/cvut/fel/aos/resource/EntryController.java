a19aca7254340e6fab9dd9401217fea5803d3628
Michal Stanke <stankmic@fel.cvut.cz>
2016-11-24 09:34:12 +0100

    JSON entry point

--- src/main/java/cz/cvut/fel/aos/resource/EntryController.java
+++ src/main/java/cz/cvut/fel/aos/resource/EntryController.java
@@ -1,12 +1,14 @@
 package cz.cvut.fel.aos.resource;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class EntryController {
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
     public String singleEntryPoint() {
         return "index";
     }
---
+++ src/main/java/cz/cvut/fel/aos/resource/RestEntryController.java
@@ -0,0 +1,40 @@
+package cz.cvut.fel.aos.resource;
+
+import com.google.common.collect.ImmutableMap;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class RestEntryController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces={MediaType.APPLICATION_JSON_VALUE})
+    public Map<String, Map<String, String>> singleEntryPoint() {
+        return ImmutableMap.of(
+                "Destination", ImmutableMap.of(
+                        "GET /destination", "Return list of all destinations.",
+                        "POST /destination", "Crete new destination and return its id.",
+                        "GET /destination/{id}", "Return destination with given id.",
+                        "PUT /destination/{id}", "Update destination with given id.",
+                        "DELETE /destination/{id}", "Delete destination with given id."
+                ),
+                "Flight", ImmutableMap.of(
+                        "GET /flight", "Return list of all flights.",
+                        "POST /flight", "Crete new flight and return its id.",
+                        "GET /flight/{id}", "Return flight with given id.",
+                        "PUT /flight/{id}", "Update flight with given id.",
+                        "DELETE /flight/{id}", "Delete flight with given id."
+                ),
+                "Reservation", ImmutableMap.of(
+                        "GET /reservation", "Return list of all reservations.",
+                        "POST /reservation", "Crete new reservation and return it with id and password filled.",
+                        "GET /reservation/{id}", "Return reservation with given id.",
+                        "PUT /reservation/{id}", "Update reservation with given id.",
+                        "DELETE /reservation/{id}", "Delete reservation with given id."
+                )
+        );
+    }
+}
