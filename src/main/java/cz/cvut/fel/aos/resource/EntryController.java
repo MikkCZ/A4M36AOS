2163233484ce9ece26f6ea7027680711fa555ca2
Michal Stanke <stankmic@fel.cvut.cz>
2016-11-24 09:31:51 +0100

    Thymeleaf example

--- src/main/java/cz/cvut/fel/aos/resource/EntryController.java
+++ src/main/java/cz/cvut/fel/aos/resource/EntryController.java
@@ -1,18 +1,12 @@
 package cz.cvut.fel.aos.resource;
 
-import com.google.common.collect.ImmutableMap;
-import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
-@RestController
-@RequestMapping("/")
+@Controller
 public class EntryController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET, produces={MediaType.APPLICATION_JSON_VALUE})
+    @RequestMapping("/")
     public String singleEntryPoint() {
         return "index";
     }
