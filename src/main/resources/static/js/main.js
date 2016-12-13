884b4c2c146010202752813f10425bc01e5d602d
Zbyněk Eiselt <eiselzby@fel.cvut.cz>
2016-12-13 22:33:11 +0100

    HTTPRequest

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,16 @@
-/**
- * Created by Zbynek on 13. 12. 2016.
- */
+
+function httpGetAsync(theUrl, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            console.log(xmlHttp.responseText);
+            callback(xmlHttp.responseText);
+        }
+    };
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous
+    xmlHttp.send(null);
+}
+
+httpGetAsync("/destination", function (cb) {
+    console.log(cb);
+})
