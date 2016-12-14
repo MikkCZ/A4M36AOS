4f7441e3c37bd8a769a7cb9e35932118237a3173
Zbyněk Eiselt <eiselzby@fel.cvut.cz>
2016-12-14 01:04:59 +0100

    JS load

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,16 +1,27 @@
 
 function httpGetAsync(theUrl, callback) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-            console.log(xmlHttp.responseText);
-            callback(xmlHttp.responseText);
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            console.log(xhr.responseText);
+            callback(xhr.responseText);
         }
     };
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous
-    xmlHttp.send(null);
+
+    xhr.open("GET", theUrl, true); // true for asynchronous
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Authorization", "Basic " + window.btoa(encodeURIComponent("admin:admin")));
+    xhr.send(null);
 }
 
-httpGetAsync("/destination", function (cb) {
-    console.log(cb);
-})
+httpGetAsync("/destination", function (data) {
+    console.log(data);
+
+    data = {
+        "1": {"ID": 1,"Name": "Praha","Latitude": 160,"Longitude": 15},
+        "2": {"ID": 1,"Name": "Berlin","Latitude": 0,"Longitude": -10},
+        "3": {"ID": 1,"Name": "London","Latitude": -13,"Longitude": 90}
+    };
+
+    var table = document.getElementById("destinations");
+});
