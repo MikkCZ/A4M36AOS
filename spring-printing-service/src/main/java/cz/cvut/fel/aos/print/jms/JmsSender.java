b61c068f5befb5c21981b8dd2ff71adaadb2d2b4
Michal Stanke <stankmic@fel.cvut.cz>
2017-01-09 13:03:43 +0100

    Single try-catch only

--- spring-printing-service/src/main/java/cz/cvut/fel/aos/print/jms/JmsSender.java
+++ spring-printing-service/src/main/java/cz/cvut/fel/aos/print/jms/JmsSender.java
@@ -1,6 +1,5 @@
 package cz.cvut.fel.aos.print.jms;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import cz.cvut.fel.aos.print.jms.port.Email;
 import lombok.AllArgsConstructor;
@@ -18,12 +17,8 @@ public class JmsSender {
     public void send(Email email) {
         try {
             Thread.sleep(5_000L);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        try {
             jmsTemplate.convertAndSend(jmsDestination, objectMapper.writeValueAsString(email));
-        } catch (JsonProcessingException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
